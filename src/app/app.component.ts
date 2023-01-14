fab64b231724b8626c8821903e541c6b65c12b53
Jakub Sobór <kupkeu@gmail.com>
2023-01-14 18:28:31 +0100

    Feat: Mark task as done or undone / Changed: handleRemove() to changeDone() / Fixed: empty 'tasks' array error on adding new task

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,10 @@
+import { ThisReceiver } from '@angular/compiler';
 import { Component } from '@angular/core';
 
 interface Task {
   id: number,
-  title: string
+  title: string,
+  done: boolean
 }
 
 @Component({
@@ -13,8 +15,9 @@ interface Task {
       <header>To do list</header>
       <div>Tasks:</div>
       <ol>
-        <li *ngFor="let task of tasks">
+        <li *ngFor="let task of tasks" class="task" [class.taskDone]="task.done">
           {{ task.title }}
+          <button (click)="changeDone(task)"> X </button>
         </li>
       </ol>
       <form 
@@ -28,8 +31,9 @@ interface Task {
           ngModel
           maxlength="30"
           minlength="5"
+          placeholder="Create new task"
           required />
-        <button type="submit" [disabled]="form.invalid">Add new task</button>
+        <button type="submit" [disabled]="form.invalid">Add</button>
         <div *ngIf="(taskDesc.errors?.['minlength'] || taskDesc.errors?.['required']) && taskDesc.dirty">
           At least 5 characters required!
         </div>
@@ -43,16 +47,23 @@ export class AppComponent {
   tasks: Task[] = [{
     id: 1,
     title: "Wash the dishes",
+    done: false
   }, {
     id: 2,
     title: "Do the laundry",
+    done: true
   }, {
     id: 3,
     title: "Buy some bread",
+    done: false
   }]
 
   handleSubmit(newTask: any) {
-    console.log(newTask.taskDesc)
-    this.tasks.push({id: this.tasks[this.tasks.length - 1].id + 1, title: newTask.taskDesc})
+    if (this.tasks.length > 0) this.tasks.push({id: this.tasks[this.tasks.length - 1].id + 1, title: newTask.taskDesc, done: false})
+    else this.tasks.push({id: 1, title: newTask.taskDesc, done: false})
+    console.log(this.tasks)
+  }
+  changeDone(event: Task) {
+    event.done = !event.done
   }
 }
